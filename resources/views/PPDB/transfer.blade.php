656b0e84369aa47c80e073e37d4f9f2402dd2cca
neet693 <neet693@gmail.com>
2024-01-18 11:33:30 +0700

    Fixing Blade dari method pembayaran

--- resources/views/PPDB/transfer.blade.php
+++ resources/views/PPDB/transfer.blade.php
@@ -1,4 +1,27 @@
 @extends('Layout.app')
 @section('content')
     Page Halaman Transfer
+    <div class="content">
+        <div class="row">
+            {{-- Student Statistics --}}
+            <div class="col-12 col-md-6 col-lg-4">
+
+                <div class="statistics-card">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-column justify-content-between align-items-center">
+                            <h5 class="content-desc">Selamat, putra anda sudah terdaftar</h5>
+
+                            <h3 class="statistics-value">{{ $student->fullname }}</h3>
+                        </div>
+
+                        <button class="btn-statistics">
+                            <img src="{{ asset('template/assets/img/global/times.svg') }}" alt="">
+                        </button>
+                    </div>
+                    <button>Bayar Sekarang</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/PPDB/tunai.blade.php
+++ resources/views/PPDB/tunai.blade.php
@@ -1,4 +1,19 @@
 @extends('Layout.app')
 @section('content')
     Page Halaman Tunai
+    <div class="content">
+        <div class="row">
+            <div class="statistics-card">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex flex-column justify-content-between align-items-center">
+                        <h5 class="content-desc">Selamat, putra anda sudah terdaftar</h5>
+                        <h3 class="statistics-value">{{ $student->fullname }}</h3>
+                        <h5 class="content-desc">Silahkan datang ke sekolah untuk melakukan pembayaran
+                            <strong>Tunai</strong> dan Konfirmasi langsung ke admin.
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
