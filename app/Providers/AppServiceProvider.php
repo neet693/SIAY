83d25ac8fa979e91e6ca7fbe55645876756f242a
unknown <neet693@gmail.com>
2024-03-27 07:27:08 +0700

    Edit AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'local' || config('app.env') === 'production') {
+        if (config('app.env') === 'local') {
+            URL::forceScheme('http');
+        } else {
             URL::forceScheme('https');
         }
     }
