0e3549c1b8a6db68af525629aec73dca1e5f1ed2
unknown <neet693@gmail.com>
2024-09-20 13:39:13 +0700

    Fixing Views dan Routes Web

--- resources/views/Admin/Dashboard/index.blade.php
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -38,60 +38,6 @@
             </div>
 
             <div class="col-12 col-md-6 col-lg-4">
-
-                <div class="statistics-card">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="d-flex flex-column justify-content-between align-items-start">
-                            <h5 class="content-desc">Total Pendaftar PPDB</h5>
-
-                            <h3 class="statistics-value">{{ $students->count() }}</h3>
-                        </div>
-
-                        <a href="{{ route('ppdb.create') }}" class="btn-statistics">
-                            <img src="{{ asset('template/assets/img/global/times.svg') }}" alt="">
-                        </a>
-                    </div>
-
-                    <div class="statistics-list">
-                        @foreach ($students->sortByDesc('created_at')->take(10) as $student)
-                            <img class="statistics-image" src="https://ui-avatars.com/api/?name={{ $student->fullname }}"
-                                alt="studentImage{{ $student->fullname }}" title="{{ $student->fullname }}">
-                        @endforeach
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="col-12 col-md-6 col-lg-4">
-
-                <div class="statistics-card">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="d-flex flex-column justify-content-between align-items-start">
-                            <h5 class="content-desc">Total Pendaftar PPDB</h5>
-
-                            <h3 class="statistics-value">{{ $students->count() }}</h3>
-                        </div>
-
-                        <a href="{{ route('ppdb.create') }}" class="btn-statistics">
-                            <img src="{{ asset('template/assets/img/global/times.svg') }}" alt="">
-                        </a>
-                    </div>
-
-                    <div class="statistics-list">
-                        @foreach ($students->sortByDesc('created_at')->take(10) as $student)
-                            <img class="statistics-image" src="https://ui-avatars.com/api/?name={{ $student->fullname }}"
-                                alt="studentImage{{ $student->fullname }}" title="{{ $student->fullname }}">
-                        @endforeach
-                    </div>
-
-                </div>
-
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-12 col-md-6 col-lg-6">
                 <div class="statistics-card">
 
                     <div class="d-flex justify-content-between align-items-center">
@@ -127,9 +73,11 @@
                     </div>
 
                 </div>
+
             </div>
 
-            <div class="col-12 col-md-6 col-lg-6">
+
+            <div class="col-12 col-md-6 col-lg-4">
                 <div class="statistics-card">
 
                     <div class="d-flex justify-content-between align-items-center">
@@ -165,9 +113,11 @@
                     </div>
 
                 </div>
+
             </div>
         </div>
 
+        <!--Table Pembayaran -->
         <div class="row mt-5">
             <div class="container">
                 <table id="transactionTable" class="table table-striped" style="width:100%; justify-content: center">
@@ -217,6 +167,83 @@
             </div>
         </div>
 
+        <!--Table Student -->
+        <div class="row mt-5">
+            <div class="container">
+                <table id="studentTable" class="table table-striped" style="width:100%; justify-content: center">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama</th>
+                            <th>Jenjang</th>
+                            <th>Tahun Ajaran</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($students as $student)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>
+                                    <a href="{{ route('admin.student.show', ['unique_code' => $student->unique_code]) }}"
+                                        style="text-decoration: none; color: black;">
+                                        {{ $student->fullname }}
+                                    </a>
+                                </td>
+                                <td>{{ $student->schoolInformation->educationLevel->level_name }}</td>
+                                <td>{{ $student->schoolInformation->academicYear->name }}</td>
+                                <td>{{ $student->status_penerimaan }}</td>
+                                <td>
+                                    @if ($student->status_penerimaan != 'Diterima')
+                                        <div class="dropdown">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false">
+                                                Pilih Aksi
+                                            </button>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <form method="POST"
+                                                        action="{{ route('admin.set.accept', ['unique_code' => $student->unique_code]) }}">
+                                                        @csrf
+                                                        <button type="submit"
+                                                            class="link-success text-decoration-none btn">Diterima</button>
+                                                    </form>
+                                                </li>
+                                                <li>
+                                                    <form method="POST" class="d-inline"
+                                                        action="{{ route('admin.set.reject', ['unique_code' => $student->unique_code]) }}">
+                                                        @csrf
+                                                        <button type="submit"
+                                                            class="link-danger text-decoration-none btn">Ditolak</button>
+                                                    </form>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    @endif
+                                    <a class="btn btn-link link-success d-inline-block"
+                                        title="Print Formulir{{ $student->fullname }}"
+                                        href="{{ route('print-formmulir-ppdb', ['unique_code' => $student->unique_code]) }}">
+                                        <i class="bi bi-printer"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama</th>
+                            <th>Jenjang</th>
+                            <th>Tahun Ajaran</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+
         <div class="row mt-5">
             <div class="col-12 col-lg-6">
                 <h2 class="content-title">Quick Links</h2>
@@ -252,34 +279,54 @@
                 <h5 class="content-desc mb-4">PPDB Student Tracker</h5>
 
                 <div class="document-card">
+                    @php
+                        // Sesuaikan dengan timezone lokal Anda
+                        $timezone = 'Asia/Jakarta'; // Ganti dengan timezone yang sesuai
+                        $oneMonthAgo = now()->setTimezone($timezone)->subMonth();
+                        $hasStudents = false; // Flag untuk mengecek ada siswa
+                    @endphp
+
                     @foreach ($students as $item)
-                        <div class="document-item">
-                            {{-- <a href="{{ route('admin.students.show', ['student' => $item->id]) }}" --}}
-                            <a href="{{ route('admin.student.show', ['unique_code' => $item->unique_code]) }}"
-                                style="text-decoration: none; color: black;">
-                                <div class="d-flex justify-content-start align-items-center">
-                                    <img class="document-icon"
-                                        src="{{ asset('template/assets/img/home/history/photo.png') }}" alt="">
+                        @if ($item->created_at > $oneMonthAgo)
+                            <div class="document-item">
+                                <a href="{{ route('admin.student.show', ['unique_code' => $item->unique_code]) }}"
+                                    style="text-decoration: none; color: black;">
+                                    <div class="d-flex justify-content-start align-items-center">
+                                        <img class="document-icon"
+                                            src="{{ asset('template/assets/img/home/history/photo.png') }}"
+                                            alt="">
 
-                                    <div class="d-flex flex-column justify-content-between align-items-start">
-                                        <h2 class="document-title">{{ $item->fullname }}</h2>
-
-                                        {{ $item->schoolInformation->academicYear->name ?? 'Not available' }}
-                                        •
-                                        {{ $item->schoolInformation->educationLevel->level_name ?? 'Not available' }}</span>
+                                        <div class="d-flex flex-column justify-content-between align-items-start">
+                                            <h2 class="document-title">{{ $item->fullname }}</h2>
+                                            {{ $item->schoolInformation->academicYear->name ?? 'Not available' }}
+                                            •
+                                            {{ $item->schoolInformation->educationLevel->level_name ?? 'Not available' }}</span>
+                                        </div>
                                     </div>
-                                </div>
-                            </a>
-                        </div>
+                                </a>
+                            </div>
+                            @php
+                                $hasStudents = true; // Set flag ke true jika ada siswa
+                            @endphp
+                        @endif
                     @endforeach
+
+                    @if (!$hasStudents)
+                        <p>Tidak ada siswa yang terdaftar dalam sebulan terakhir.</p>
+                    @endif
                 </div>
             </div>
+
+
         </div>
     </div>
 
     <script>
         $(document).ready(function() {
             $('#transactionTable').DataTable(); // #example adalah ID tabel yang ingin Anda terapkan DataTables
+        });
+        $(document).ready(function() {
+            $('#studentTable').DataTable(); // #example adalah ID tabel yang ingin Anda terapkan DataTables
         });
     </script>
 @endsection
