ae357d121ec74bcaf72a6a8e0a94a111e5b405ca
unknown <neet693@gmail.com>
2025-08-08 09:39:09 +0700

    Penamabahan No Telp TU dan HUMAS per unit di SPMB

--- resources/views/PPDB/success.blade.php
+++ resources/views/PPDB/success.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <section class="checkout">
         <div class="container">
-            <div class="row text-center">
+            <div class="text-center row">
                 <div class="col-lg-12 col-12">
                     <img src="{{ asset('app/assets/images/ill_register.png') }}" height="300" class="mb-5" alt=" ">
                 </div>
@@ -16,11 +16,13 @@
                     <h5 class="content-desc">Silahkan Transfer ke rekening berikut BCA <strong>0083481002 </strong>
                         a.n <strong>YAYASAN PENDIDIKAN KRISTEN YAHYA </strong></h5>
                     <h5 class="content-desc">Berikut ini adalah nomor kontak yang dapat Anda hubungi untuk proses pengiriman
-                        bukti transfer dan konfirmasi pembayaran:
-                        <strong>0813-2210-8575</strong> dan
-                        <strong>0852-9011-8600</strong>.
+                        bukti transfer dan konfirmasi pembayaran: <br>
+                        TU {{ $student->schoolInformation->educationLevel->level_name }}:
+                        {{ $student->schoolInformation->educationLevel->phone_tu }} <br>
+                        HUMAS {{ $student->schoolInformation->educationLevel->level_name }}:
+                        {{ $student->schoolInformation->educationLevel->phone_humas }}.
                     </h5>
-                    <a class="btn btn-master btn-warning text-white "
+                    <a class="text-white btn btn-master btn-warning "
                         href="{{ route('print-formmulir-ppdb', ['unique_code' => $student->unique_code]) }}">
                         Print Formulir PPDB
                     </a>
